29625e150290e1bfc9443a810be3d9afe2a41f4d
altaibayar tseveenbayar <altai@bayar.me>
2019-07-13 23:13:31 +0200

    371

---
+++ src/LeetCodeTest/Problems/301_400/371.swift
@@ -0,0 +1,17 @@
+//
+//  371.swift
+//  LeetCodeTest
+//
+//  Created by altaibayar tseveenbayar on 2019-07-13.
+//  Copyright © 2019 tsevealt. All rights reserved.
+//
+
+import Foundation
+
+/* Java
+
+public int getSum(int a, int b) {
+    return ((a&b)<<1) + (a^b);
+}
+
+ */
