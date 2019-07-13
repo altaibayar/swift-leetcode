e6997bceb8318feddb60747a60147781d0925fdc
altaibayar tseveenbayar <altai@bayar.me>
2019-07-13 22:56:23 +0200

    172

---
+++ src/LeetCodeTest/Problems/101_200/172.swift
@@ -0,0 +1,23 @@
+//
+//  172.swift
+//  LeetCodeTest
+//
+//  Created by altaibayar tseveenbayar on 2019-07-13.
+//  Copyright © 2019 tsevealt. All rights reserved.
+//
+
+import Foundation
+
+/* Java
+
+ public int trailingZeroes(int n) {
+     int result = 0;
+
+     while (n / 5 > 0) {
+         result += (n / 5);
+         n /= 5;
+     }
+
+     return result;
+ }
+ */
