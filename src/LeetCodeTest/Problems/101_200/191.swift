019f1385b114a4ac0fc2a2c90b64cce9c7614e13
altaibayar tseveenbayar <altai@bayar.me>
2019-07-13 23:04:34 +0200

    191

---
+++ src/LeetCodeTest/Problems/101_200/191.swift
@@ -0,0 +1,21 @@
+//
+//  191.swift
+//  LeetCodeTest
+//
+//  Created by altaibayar tseveenbayar on 2019-07-13.
+//  Copyright © 2019 tsevealt. All rights reserved.
+//
+
+import Foundation
+
+/* Python
+
+ def hammingWeight(self, n):
+     result = 0
+     while n > 0:
+        if n % 2 == 1:
+            result += 1
+        n = n / 2
+
+     return result
+ */
