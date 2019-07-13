8ed66c3fbb8d6625c2e7f99f61d2d06946d60d61
altaibayar tseveenbayar <altai@bayar.me>
2019-07-13 22:50:25 +0200

    160, 171

---
+++ src/LeetCodeTest/Problems/101_200/160.swift
@@ -0,0 +1,41 @@
+//
+//  160.swift
+//  LeetCodeTest
+//
+//  Created by altaibayar tseveenbayar on 2019-07-13.
+//  Copyright © 2019 tsevealt. All rights reserved.
+//
+
+import Foundation
+
+class Problem_160: ProblemProtocol {
+
+    /* Done in Java
+
+     public ListNode getIntersectionNode(ListNode headA, ListNode headB) {
+         HashSet<ListNode> set = new HashSet<ListNode>();
+
+         ListNode a = headA;
+         while (a != null) {
+             set.add(a);
+             a = a.next;
+         }
+
+         ListNode b = headB;
+         while (b != null) {
+             if (set.contains(b)) {
+                return b;
+             }
+
+             b = b.next;
+         }
+
+         return null;
+     }
+
+     */
+
+    func run() {
+
+    }
+}
---
+++ src/LeetCodeTest/Problems/101_200/171.swift
@@ -0,0 +1,40 @@
+//
+//  171.swift
+//  LeetCodeTest
+//
+//  Created by altaibayar tseveenbayar on 2019-07-13.
+//  Copyright © 2019 tsevealt. All rights reserved.
+//
+
+import Foundation
+
+class Problem_171: ProblemProtocol {
+    /* Java
+
+     public int titleToNumber(String s) {
+         if (s == null) { return 0; }
+
+         byte offsetA = (byte)'A';
+
+         int result = 0;
+
+         for (int i = 0; i < s.length(); i ++) {
+             byte ch = (byte)s.charAt(i);
+
+             int val = (ch - offsetA) + 1;
+             result *= 26;
+             result += val;
+         }
+
+         return result;
+     }
+
+     */
+
+
+    func run() {
+
+    }
+
+
+}
