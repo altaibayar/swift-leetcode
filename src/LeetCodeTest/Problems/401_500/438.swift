7cdfe54b44c0293d6a29d9c35850ee1fba658f7e
altai <altai@bayar.me>
2019-07-23 13:43:46 +0200

    428

--- src/LeetCodeTest/Problems/401_500/438.swift
+++ src/LeetCodeTest/Problems/401_500/438.swift
@@ -9,17 +9,14 @@
 import Foundation
 
 class Problem_438: ProblemProtocol {
-
-    func thumbprint(str: String) -> String {
-        var result = Array(repeating: 0, count: 26)
-        let aOffset = "a".first!.asciiValue!
-
+    
+    func makeMap(str: String) -> [Int] {
+        var result = [Int](repeating: 0, count: 26)
         for ch in str {
-            let idx: Int = Int(ch.asciiValue! - aOffset)
-            result[idx] += 1
+            result[ch.index] += 1
         }
         
-        return result.map { "\($0)" }.joined(separator: "_")
+        return result
     }
     
     func findAnagrams(_ s: String, _ p: String) -> [Int] {
@@ -28,23 +25,83 @@ class Problem_438: ProblemProtocol {
         }
 
         var result = [Int]()
+        var map = makeMap(str: p)
         
-        let thumb = thumbprint(str: p)
-        for i in 0 ..< s.count - p.count {
-            let sub = s[i ... i + p.count]
-            if thumb == thumbprint(str: sub) {
-                result.append(i)
+        var missing = p.count
+        var j = 0
+        
+        for (i, ch) in s.enumerated() {
+            let idx: Int = ch.index
+            if map[idx] > 0 {
+                missing -= 1
             }
             
+            map[idx] -= 1
+            
+            while missing == 0 {
+                if i - j + 1 == p.count {
+                    result.append(j)
+                }
+                
+                let sj = s[s.index(s.startIndex, offsetBy: j)]
+                map[sj.index] += 1
+                if map[sj.index] > 0 {
+                    missing += 1
+                }
+                
+                j += 1
+            }
         }
         
         return result
     }
 
+    /*
+     public List<Integer> findAnagrams(String s, String p) {
+     
+        char[] sc = s.toCharArray();
+        char[] pc = p.toCharArray();
+        int[] map = new int[26];
+        List<Integer> ans = new ArrayList<>();
+        int len = pc.length;
+        for(char c:pc)
+            map[c-'a']++;
+    
+        int j = 0;
+        int missing=pc.length;
+    
+        for(int i =0;i<sc.length;i++){
+            char c = sc[i];
+            if(map[c-'a']>0)
+                missing--;
+    
+            map[c-'a']--;
+    
+            while(missing==0){
+                if(i-j+1==len)
+                    ans.add(j);
+    
+                if(++map[sc[j]-'a']>0)
+                    missing++;
+    
+                j++;
+            }
+        }
+     
+        return ans;
+     }
+     */
+    
     func run() {
         print(findAnagrams("", ""))
         print(findAnagrams("", "ab"))
         print(findAnagrams("cbaebabacd", "abc"))
         print(findAnagrams("abab", "ab"))
+    }
+}
+
+private extension Character {
+    var index: Int {
+        return Int(self.asciiValue! - "a".first!.asciiValue!)
     }
 }
