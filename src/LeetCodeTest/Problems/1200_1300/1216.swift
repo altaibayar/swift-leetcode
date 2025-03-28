f64881c6705dcc77265f06bfae464643a0e09803
altaibayar <altai@bayar.me>
2025-03-28 15:38:57 +0000

    1216

--- src/LeetCodeTest/Problems/1200_1300/1216.swift
+++ src/LeetCodeTest/Problems/1200_1300/1216.swift
@@ -7,9 +7,38 @@
 //
 
 class Problem_1216: ProblemProtocol {
+    func isPalindrome(memo: inout [[Int?]], chars: inout [Character], i: Int, j: Int) -> Int {
+        if i == j {
+            return 0
+        }
+        
+        if i == j - 1 {
+            return chars[i] != chars[j] ? 1 : 0
+        }
+        
+        if let mem = memo[i][j] {
+            return mem
+        }
+        
+        if chars[i] == chars[j] {
+            let mem = isPalindrome(memo: &memo, chars: &chars, i: i + 1, j: j - 1)
+            memo[i][j] = mem
+            return mem
+        }
+        
+        let mem = 1 + min(
+            isPalindrome(memo: &memo, chars: &chars, i: i + 1, j: j),
+            isPalindrome(memo: &memo, chars: &chars, i: i, j: j - 1)
+        )
+        memo[i][j] = mem
+        
+        return mem
+    }
     
     func isValidPalindrome(_ s: String, _ k: Int) -> Bool {
-        true
+        var memo = [[Int?]](repeating: [Int?](repeating: nil, count: s.count), count: s.count)
+        var chars = s.map { $0 }
+        return isPalindrome(memo: &memo, chars: &chars, i: 0, j: s.count - 1) <= k
     }
     
     func run() {
@@ -17,3 +46,4 @@ class Problem_1216: ProblemProtocol {
         print(isValidPalindrome("abbababa", 1))
     }
 }
+
