0c570bcb0aed8a956fe14735311b935c392c41cd
altaibayar <altai@bayar.me>
2025-04-01 18:01:37 +0100

    54

--- src/LeetCodeTest/Problems/1_100/54.swift
+++ src/LeetCodeTest/Problems/1_100/54.swift
@@ -9,6 +9,48 @@
 class Problem_54: ProblemProtocol {
     func spiralOrder(_ matrix: [[Int]]) -> [Int] {
         var result = [Int]()
+        
+        let row = matrix.count, column = matrix[0].count
+        var left = 0, top = 0, right = column - 1, bottom = row - 1
+        
+        while row * column > result.count {
+            // R
+            for x in left ... right {
+                result.append(matrix[top][x])
+            }
+            
+            // D
+            if top + 1 <= bottom {
+                for y in top + 1 ... bottom {
+                    result.append(matrix[y][right])
+                }
+            }
+                
+            // L
+            if top < bottom {
+                for x in stride(from: right - 1, through: left, by: -1) {
+                    result.append(matrix[bottom][x])
+                }
+            }
+            
+            // U
+            if left < right {
+                for y in stride(from: bottom - 1, to: top, by: -1) {
+                    result.append(matrix[y][left])
+                }
+            }
+            
+            bottom -= 1
+            top += 1
+            left += 1
+            right -= 1
+        }
+
+        return result
+    }
+    
+    func _spiralOrder(_ matrix: [[Int]]) -> [Int] {
+        var result = [Int]()
 
         let rows = matrix.count, columns = matrix[0].count
         var up = 0, down = rows - 1, left = 0, right = columns - 1
--- src/LeetCodeTest/main.swift
+++ src/LeetCodeTest/main.swift
@@ -8,4 +8,4 @@
 
 import Foundation
 
-Problem_1216().run();
+Problem_54().run();
