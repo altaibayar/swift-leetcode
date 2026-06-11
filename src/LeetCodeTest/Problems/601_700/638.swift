55fb56950d35803686adcf4cb7482f7676dd8fe8
altai <altai@bayar.me>
2026-06-11 13:23:17 +0100

    638

--- src/LeetCodeTest/Problems/601_700/638.swift
+++ src/LeetCodeTest/Problems/601_700/638.swift
@@ -9,49 +9,62 @@
 class Problem_638: ProblemProtocol {
 
     func shoppingOffers(_ price: [Int], _ special: [[Int]], _ needs: [Int]) -> Int {
-        var j = 0
-        var res = dot(needs, price)
+        var cache = [String: Int]()
 
-        for s in special {
-            var needsCopy = needs
+        func dfs(_ currentNeeds: [Int]) -> Int {
+            let key = currentNeeds.map { String($0) }.joined(separator: ",")
+            if let result = cache[key] {
+                return result
+            }
 
-            j = 0
-            while j < needs.count {
+            if currentNeeds.allSatisfy({ $0 == 0 }) {
+                return 0
+            }
 
-                let diff = needsCopy[j] - s[j]
-                if diff < 0 {
-                    break
+            var cost = zip(currentNeeds, price).reduce(0) { $0 + ($1.0 * $1.1) }
+
+            let applyFirstApplicableOffer: ([Int]) -> [Int]? = { offer in
+                var newNeeds = currentNeeds
+
+                for i in 0..<price.count {
+                    if newNeeds[i] < offer[i] {
+                        return nil
+                    }
+
+                    newNeeds[i] -= offer[i]
                 }
 
-                needsCopy[j] = diff
+                return newNeeds
+            }
 
-                j += 1
+            for offer in special {
+                if let newNeeds = applyFirstApplicableOffer(offer) {
+                    cost = min(cost, offer.last! + dfs(newNeeds))
+                }
             }
-            
-            if j == needs.count {
-                res = min (
-                    res,
-                    s[j] + shoppingOffers(price, special, needsCopy)
-                )
-            }
+
+            cache[key] = cost
+            return cost
         }
 
-        return res
-    }
-
-    func dot(_ aa: [Int], _ bb: [Int]) -> Int {
-        var result = 0
-
-        for (a, b) in zip(aa, bb) {
-            result += (a * b)
-        }
-
-        return result
+        return dfs(needs)
     }
 
     func run() {
         print(shoppingOffers([2, 5], [[3, 0, 5], [1, 2, 10]], [3, 2]))
         print(shoppingOffers([2, 3, 4], [[1, 1, 0, 4], [2, 2, 1, 9]], [1, 2, 1]))
+
+        print(
+            shoppingOffers(
+                [2, 2],
+                [
+                    [1, 1, 1], [1, 1, 2], [1, 1, 3], [1, 1, 4], [1, 1, 5], [1, 1, 6], [1, 1, 7],
+                    [1, 1, 8], [1, 1, 9], [1, 1, 10], [1, 1, 11], [1, 1, 12], [1, 1, 13],
+                    [1, 1, 14], [1, 1, 15],
+                ],
+                [10, 10]
+            )
+        )
     }
 
 }
